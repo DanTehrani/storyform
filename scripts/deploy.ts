d9313968be7b8435e3721877a20ee2e937daef93
Daniel Tehrani <contact@dantehrani.com>
2022-08-02 16:40:47 +0900

    Setup eslint and prettier

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -5,24 +5,28 @@ async function main() {
   const verifier = await Verifier.deploy();
   await verifier.deployed();
 
-  const SemaphoreVerifier = await ethers.getContractFactory("SemaphoreVerifier16");
+  const SemaphoreVerifier = await ethers.getContractFactory(
+    "SemaphoreVerifier16"
+  );
 
   const semaphoreVerifier = await SemaphoreVerifier.deploy();
   await semaphoreVerifier.deployed();
 
-
-  console.log(`Deployed SemaphoreVerifier to: ${semaphoreVerifier.address}`)
+  console.log(`Deployed SemaphoreVerifier to: ${semaphoreVerifier.address}`);
 
   const StoryForm = await ethers.getContractFactory("StoryForm");
-  const storyForm = await StoryForm.deploy(verifier.address, semaphoreVerifier.address);
+  const storyForm = await StoryForm.deploy(
+    verifier.address,
+    semaphoreVerifier.address
+  );
   await storyForm.deployed();
 
-  console.log(`Deployed StoryForm to: ${storyForm.address}`)
+  console.log(`Deployed StoryForm to: ${storyForm.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main().catch(error => {
   console.error(error);
   process.exitCode = 1;
 });
