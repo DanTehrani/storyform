4127bae3f50689496b8d46884d28613a411c9b89
Daniel Tehrani <contact@dantehrani.com>
2022-08-04 16:20:17 +0900

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,57 @@
+import { poseidon_gencontract as poseidonContract } from "circomlibjs";
 import { ethers } from "hardhat";
 
 async function main() {
-  const Verifier = await ethers.getContractFactory("Verifier");
-  const verifier = await Verifier.deploy();
-  await verifier.deployed();
+  // First, deploy the following libraries
+  // - PoseidonT3
+  // - IncrementalBinaryTree
+  const poseidonABI = poseidonContract.generateABI(2);
+  const poseidonBytecode = poseidonContract.createCode(2);
+
+  const [signer] = await ethers.getSigners();
+
+  const PoseidonLibFactory = new ethers.ContractFactory(
+    poseidonABI,
+    poseidonBytecode,
+    signer
+  );
+  const poseidonLib = await PoseidonLibFactory.deploy();
+  await poseidonLib.deployed();
+
+  const IncrementalBinaryTreeLibFactory = await ethers.getContractFactory(
+    "IncrementalBinaryTree",
+    {
+      libraries: {
+        PoseidonT3: poseidonLib.address
+      }
+    }
+  );
+  const incrementalBinaryTreeLib =
+    await IncrementalBinaryTreeLibFactory.deploy();
+  await incrementalBinaryTreeLib.deployed();
 
   const SemaphoreVerifier = await ethers.getContractFactory(
     "SemaphoreVerifier16"
   );
-
   const semaphoreVerifier = await SemaphoreVerifier.deploy();
   await semaphoreVerifier.deployed();
 
-  console.log(`Deployed SemaphoreVerifier to: ${semaphoreVerifier.address}`);
+  const Verifier = await ethers.getContractFactory("Verifier");
+  const verifier = await Verifier.deploy();
+  await verifier.deployed();
 
-  const StoryForm = await ethers.getContractFactory("StoryForm");
+  const StoryForm = await ethers.getContractFactory("StoryForm", {
+    libraries: {
+      IncrementalBinaryTree: incrementalBinaryTreeLib.address
+    }
+  });
   const storyForm = await StoryForm.deploy(
     verifier.address,
     semaphoreVerifier.address
   );
   await storyForm.deployed();
 
-  console.log(`Deployed StoryForm to: ${storyForm.address}`);
+  console.log(`Deployed StoryForm to ${storyForm.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
