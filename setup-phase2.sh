b491fc3ad9323d2a088a9b38f80b03e1b5318194
Daniel Tehrani <contact@dantehrani.com>
2022-10-01 21:06:48 +0900

    Update repo context

--- setup-phase2.sh
+++ setup-phase2.sh
@@ -1,6 +1,6 @@
 # $1: Circuit name
 # $2: Ptau file power
 
-snarkjs groth16 setup ./circuits/$1/$1.r1cs powersOfTau28_hez_final_$2.ptau ./circuits/$1/keys/$1.zkey
-snarkjs zkey contribute ./circuits/$1/keys/$1.zkey ./circuits/$1/keys/$1_1.zkey --name="1st Contributor Name" -v
-snarkjs zkey export verificationkey ./circuits/$1/keys/$1_1.zkey ./circuits/$1/keys/verification_key.json
+yarn snarkjs groth16 setup ./circuits/$1.r1cs powersOfTau28_hez_final_$2.ptau ./circuits/keys/$1.zkey
+yarn snarkjs zkey contribute ./circuits/keys/$1.zkey ./circuits/keys/$1_1.zkey --name="1st Contributor Name" -v
+yarn snarkjs zkey export verificationkey ./circuits/keys/$1_1.zkey ./circuits/keys/verification_key.json
